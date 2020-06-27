e87fead1544a3c1cd88d33262a3bb0ad5f1187ec
Patrick Thompson <patrick@inkstonesoftware.com>
2020-06-27 08:57:03 -0700

    Simplify push event example
    - Patrick Thompson: Simplify to use registered hook to push events.

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,21 +14,9 @@ Spruce.store('application', {
 })
 // Define hooks
 const Hooks = {}
-Hooks.LiveViewPushEventHook = {
+Hooks.PushEvent = {
   mounted() {
-    const liveView = this
-    this.liveViewPushEvent = function(e) {
-      liveView.pushEvent(e.detail.event, e.detail.payload)
-    }
-    this.liveViewPushEventTo = function(e) {
-      liveView.pushEventTo(e.detail.selector, e.detail.event, e.detail.payload)
-    }
-    window.addEventListener('liveview-push-event', this.liveViewPushEvent)
-    window.addEventListener('liveview-push-event-to', this.liveViewPushEventTo)
-  },
-  destroyed() {
-    window.removeEventListener('liveview-push-event', this.liveViewPushEvent)
-    window.removeEventListener('liveview-push-event-to', this.liveViewPushEventTo)
+    window.pushEventHook = this
   }
 }
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
