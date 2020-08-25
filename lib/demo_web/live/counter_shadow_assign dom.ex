7cfe4c85a86a23acdd62ce9fcab44dd9c713b167
Patrick Thompson <patrick@inkstonesoftware.com>
2020-08-25 08:59:37 -0700

    Formatting

--- lib/demo_web/live/counter_shadow_assign dom.ex
+++ lib/demo_web/live/counter_shadow_assign dom.ex
@@ -16,19 +16,19 @@ defmodule DemoWeb.CounterShadowAssignDomLive do
   def render(assigns) do
     ~L"""
     <div id="counter"
-      x-data="{arr: [], count: <%= @count %>}"
-      x-init="() => {
-        $watch('count', value => {
-          arr = Array.from({length:value},(v,k)=>k+1)
-        })
-      }">
-    <h1>The assigns count is: <span><%= @count %></span></h1>
-    <h1>The alpine count is: <span x-text="count"></span></h1>
-    <button phx-click="decrement"> Decrement </button>
-    <button phx-click="increment"> Increment </button>
-    <template x-for="item in arr" :key="item">
-    <div x-text="item"></div>
-    </template>
+        x-data="{arr: [], count: <%= @count %>}"
+        x-init="() => {
+          $watch('count', value => {
+            arr = Array.from({length:value},(v,k)=>k+1)
+          })
+        }">
+      <h1>The assigns count is: <span><%= @count %></span></h1>
+      <h1>The alpine count is: <span x-text="count"></span></h1>
+      <button phx-click="decrement"> Decrement </button>
+      <button phx-click="increment"> Increment </button>
+      <template x-for="item in arr" :key="item">
+        <div x-text="item"></div>
+      </template>
     </div>
     """
   end
